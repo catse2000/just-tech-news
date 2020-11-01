bb6ec7d75a157e335448a49f12961516089ce526
catse2000 <catse2000@yahoo.com>
2020-11-01 13:11:35 -0600

    added login, logout and cookies

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars'); 
 const hbs = exphbs.create({});
 
+
 const app = express();
 const PORT = process.env.PORT || 3003;
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 //Setup Handlebars.js
 app.engine('handlebars', hbs.engine);
